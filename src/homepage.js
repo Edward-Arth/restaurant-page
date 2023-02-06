8800307a33ac5827a6336d58b07c505624528ebe
Edward-Arth <116842980+Edward-Arth@users.noreply.github.com>
2023-02-06 16:46:31 -0500

    Refined homepage and added buttons

--- src/homepage.js
+++ src/homepage.js
@@ -12,6 +12,7 @@ export default function homepageGen () {
     
     const description = document.createElement("description");
     description.id = "description";
+    description.classList.add("homepageDesc");
     homepage.appendChild(description);
     description.textContent = "Diane's distributes despondent desserts. Patrons prefer pain; Diane delivers."
 
@@ -22,20 +23,24 @@ export default function homepageGen () {
     const img3 = document.createElement("img");
     img3.src = "./images/dish2.jpg";
     img3.id = "dish2"
+    img3.classList.add("homepagePics")
     imageHolder.appendChild(img3);
     
     const img = document.createElement("img");
     img.src = "./images/restaurant.jpg";
     img.id = "restaurant"
+    img.classList.add("homepagePics");
     imageHolder.appendChild(img);
 
     const img2 = document.createElement("img");
     img2.src = "./images/dish1.webp";
     img2.id = "dish1";
+    img2.classList.add("homepagePics");
     imageHolder.appendChild(img2);
     
     const description2 = document.createElement("description2");
     description2.id = "description2";
+    description2.classList.add("homepageDesc");
     homepage.appendChild(description2);
     description2.textContent = "Try the tasteless tartufo, or our ornery oreo oatmeal..."
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import homepageGen from './homepage.js';
 
-homepageGen ();
+const content = document.getElementById("content");
 
+const buttons = document.createElement("div")
+buttons.id = "buttons";
+content.appendChild(buttons);
+
+const menu = document.createElement("button");
+menu.id = "menu";
+menu.classList.add("myButtons");
+menu.textContent = "Menu";
+buttons.appendChild(menu);
+
+const contact = document.createElement("button");
+contact.id = "contact";
+contact.classList.add("myButtons");
+contact.textContent = "Contact";
+buttons.appendChild(contact);
+
+homepageGen ();
